0e564d1bc4cbac23e79d2d00a3a13bcbb1770f0e
sahibtt <sahibtt@code.edu.az>
2022-02-13 19:13:46 +0400

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("neche imtahan oldugunu daxil edin, sonra ise imtahanlar ballarini sira ile daxil edin");
+            string imtahansayi = Console.ReadLine();
+            int say = int.Parse(imtahansayi);
+            int[] arr = new int[say];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+            step1:
+                Console.WriteLine("imtahan ballarini daxil edin");
+                string b = Console.ReadLine();
+                int points = int.Parse(b);
+                arr[i] = points;
+            step2:
+                if (points > 100)
+                {
+                    Console.WriteLine("imtahan neticesi 100den boyuk ola bilmez, yeniden daxil edin");
+                    goto step1;
+                }
+
+
+            }
+
+
+            if (calcavg(arr, say) > 60)
+            {
+                Console.WriteLine("mezun oldunuz");
+
+            }
+            else
+            {
+                Console.WriteLine("mezun ola bilmediniz");
+
+
+
+            }
+
+
+
+        }
+        static int calcavg(int[] arr, int points)
+        {
+            int examsum = 0;
+            int average = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                examsum += arr[i];
+            }
+            average = examsum / arr.Length;
+
+            return average;
+
+
+
+        }
+    }
+}
